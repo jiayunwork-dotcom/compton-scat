c3ca86f25f0da9d8a1494f5e83b15b889a8c769a
“jiayun” <jiayun_work@163.com>
2026-08-22 15:13:49 +0800

    fix: keep Klein-Nishina differential at 90 deg instead of sharing scratch and zeroing it

--- internal/section/knscratch.go
+++ internal/section/knscratch.go
@@ -1,14 +1,9 @@
 package section
 
-var knScratch float64
-
-func shareKN(v *float64) *float64 {
+func shareKN(v float64) float64 {
 	return v
 }
 
 func fillKN(v float64) float64 {
-	knScratch = v
-	out := shareKN(&knScratch)
-	*out = 0
-	return *out
+	return shareKN(v)
 }
