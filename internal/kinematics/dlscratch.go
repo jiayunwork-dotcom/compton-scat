09a0ab5255ae2e2bb286eace79ad7c8319221dd7
“jiayun” <jiayun_work@163.com>
2026-08-22 15:13:49 +0800

    fix: keep Compton wavelength shift instead of sharing scratch and zeroing it

--- internal/kinematics/dlscratch.go
+++ internal/kinematics/dlscratch.go
@@ -1,14 +1,9 @@
 package kinematics
 
-var dlScratch float64
-
-func shareDL(v *float64) *float64 {
+func shareDL(v float64) float64 {
 	return v
 }
 
 func fillDL(v float64) float64 {
-	dlScratch = v
-	out := shareDL(&dlScratch)
-	*out = 0
-	return *out
+	return shareDL(v)
 }
